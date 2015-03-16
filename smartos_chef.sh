506f11df8f398f07921e7704b00985e16b9a8e56
Dinu <dinu@tcnbroadcasting.com>
2015-03-17 00:00:57 +0200

    Initial

---
+++ smartos_chef.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pkgin -f update
+for package in gcc47 git gmake ruby200-base ruby200-yajl ruby200-nokogiri ruby200-readline pkg-config; do
+  pkg_info $package >/dev/null 2>&1
+  if [ $? -gt 0 ]; then
+    pkgin -y install $package
+  else
+    echo "$package already installed ... skipping"
+  fi
+done
+  
+export PATH=/opt/local/gnu/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
+
+gem update --system
+
+gem install --no-ri --no-rdoc --force ohai
+gem install --no-ri --no-rdoc --force chef
+gem install --no-ri --no-rdoc rb-readline
+
+mkdir -p /etc/chef
+
+
