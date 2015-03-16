780f57f9aad918c707690826742f801d0e01e7a5
Dinu <dinu@tcnbroadcasting.com>
2015-03-17 00:17:21 +0200

    Added linking to chef binaries and removed chef_omnibus_root to satsify kitchen's require_chef_omnibus: true setting

--- smartos_chef.sh
+++ smartos_chef.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkgin -f update
-for package in gcc47 git gmake ruby200-base ruby200-yajl ruby200-nokogiri ruby200-readline pkg-config; do
+for package in gcc47 gmake ruby200-base ruby200-yajl ruby200-nokogiri ruby200-readline pkg-config; do
   pkg_info $package >/dev/null 2>&1
   if [ $? -gt 0 ]; then
     pkgin -y install $package
@@ -17,6 +17,10 @@ gem install --no-ri --no-rdoc --force ohai
 gem install --no-ri --no-rdoc --force chef
 gem install --no-ri --no-rdoc rb-readline
 
-mkdir -p /etc/chef
+# mkdir -p /etc/chef
+mkdir -p /opt/chef/bin
+for bin in $(ls /opt/local/bin/chef*) /opt/local/bin/ohai /opt/local/bin/knife; do 
+	ln -sf $bin /opt/chef/bin/$(basename $bin)
+done
 
 
